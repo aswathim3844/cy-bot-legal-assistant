0aefaa3064e32574a2db765900b8cecc2d7d4e6f
aswathim3844 <aswathim421@gmail.com>
2025-11-13 22:39:50 +0530

    Run app on specified host and port

--- app.py
+++ app.py
@@ -249,4 +249,5 @@ def uploaded_file(filename):
 # Add this to the BOTTOM of your app.py
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0',port=port)
     
